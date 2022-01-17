c8e7471a40815e255d520334a7fa8a5754c597fc
anupsarvade <anupsarvade@localhost>
2022-01-16 21:18:26 -0700

    Second changes push

--- eCommerce/src/main/java/com/example/eCommerce/controller/ProductController.java
+++ eCommerce/src/main/java/com/example/eCommerce/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.eCommerce.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.eCommerce.entity.Product;
+import com.example.eCommerce.entity.User;
 import com.example.eCommerce.respository.ProductRepo;
 
 @RestController
@@ -23,6 +25,12 @@ public class ProductController {
 		List<Product> ls = productRepo.findAll();
 		
 		return ls;
+	}
+	
+	@GetMapping("/byid")
+	public Optional<Product> getUserById1() {
+
+		return productRepo.findById(2);
 	}
 
 }
