84097cd95a4cdd1b7f1472acfb1d3a5c759f8ee3
anoopsarvade@gmail.com <anoopsarvade@gmail.com>
2022-02-05 23:01:26 -0700

    closing the eclipse and terminal testing

--- eCommerce/src/main/java/com/example/eCommerce/ECommerceApplication.java
+++ eCommerce/src/main/java/com/example/eCommerce/ECommerceApplication.java
@@ -10,6 +10,8 @@ public class ECommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
 		
+		// Spring boot project
+		
 	}
 
 }
